932f9459276c5c1c77ebced559bcb8a68ab7e53c
nachivpn <nachivpn@gmail.com>
2013-08-01 22:43:06 +0530

    basic 2

--- code.c
+++ code.c
@@ -2,7 +2,19 @@
 #include<stdlib.h>
 
 #define BUFFER 1000000
+
 char* text;
+size_t text_len;
+
+struct object
+{
+	int pos;	
+	char* str;
+	int ascii_sum;
+	int ascii_prod;
+	struct object* next;
+}*DS[30];	
+	
 void error()
 {
 	printf("Error!");
@@ -16,7 +28,7 @@ void text_scan()
 	text = malloc((BUFFER+1)*sizeof(char));
 	if(fp==NULL)
 		error();
-	size_t text_len = fread(text,sizeof(char),BUFFER,fp);
+	text_len = fread(text,sizeof(char),BUFFER,fp);
 	if(text_len == 0)
 		error();
 	else
@@ -24,12 +36,23 @@ void text_scan()
 	fclose(fp);
 }
 
+int str_copy(char* a, char* b)
+{
+	int i=0;
+	while(b[i]!='\0')
+	{
+		a[i]=b[i];
+		i++;
+	}
+	return i-1;
+}
+
 void print(char* array)
 {
 	int i=0;
 	while(array[i]!='\0')
 	{
-		printf("%c",text[i]);
+		printf("%c",array[i]);
 		i++;
 	}
 	printf("\n");
@@ -44,26 +67,89 @@ size_t str_len(char* array)		//returns sizeof string (type : size_t)
 	return i;
 }
 
-void remove_bull_in_text(char* array)	//Ensures all letters are converted to lowercase letters and other characters replaced by $ 
+void text_process()	//Ensures all letters are converted to lowercase letters and other characters replaced by $ 
 {
 	int i=0;
-	while(array[i]!='\0')
+	while(text[i]!='\0')
 	{
-		int ascii = (int)array[i];		
+		int ascii = (int)text[i];		
 		if(!(ascii >=97 && ascii <=122))
 			if(ascii>=65 && ascii <=90)
-				array[i]+=32;
+				text[i]+=32;
 			else
-				array[i]='$';
+				text[i]='$';
 		i++;
 	} 
 }
 
+void append_DS(int length, char* string, int pos)
+{
+	struct object* a = malloc(sizeof(struct object));
+	a->pos=pos;
+	a->str=string;
+	if(DS[length]!=NULL)
+	{
+		
+		a->next=DS[length];
+		DS[length]=a;
+	}
+	else
+	{
+		DS[length]=a;
+		DS[length]->next=NULL;
+	}
+	free(a);	
+}
+
+void construct_DS()
+{
+	int i=0;
+	while(text[i]!='\0')
+	{		
+		if(text[i]!='$')
+		{			
+			char *temp_string;
+			temp_string=(char *)malloc(30*sizeof(char));
+			int pos=i;			
+			while(text[i]!='$')
+			{				
+				temp_string[i-pos]=text[i];
+				i++;
+			}
+			temp_string[i-pos]='\0';
+			//print(temp_string);
+			append_DS(i,string,pos);
+		}
+		else 
+			i++;
+	}
+}
+
+
+void print_DS()
+{
+	int i;
+	for(i=1;i<30;i++)
+	{
+		struct object* temp = DS[i];
+		while(temp!=NULL)
+		{
+			print(temp->str);
+			printf(" ");
+			temp=temp->next;
+		}
+		printf("\nNEXT: ");
+	}
+}			
+		
+
 int main()
 {
 	text_scan();
-	remove_bull_in_text(text);
-	print(text);
+	text_process();
+	//print(text);
+	construct_DS();
+	//print_DS();
 	return 1;
 }
 
